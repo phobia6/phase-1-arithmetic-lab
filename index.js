7a315e9e8ea859f074746ca4a17531c0cd2f7a07
TohilGuerra <tohil6@gmail.com>
2023-03-19 13:17:32 -0500

    adds arithmetics

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
-
 // Write your code here
+/* let num1,num2;
+function multiply(num1,num2) {
+    return num1 * num2;
+}
+multiply(7,9); */
+const num1 = 31;
+const num2 = 2;
+const num3 = 17;
+const num4 = 13;
+const multiply = num1 * num2;
+
+const random = Math.floor(Math.random() * 10) + 1;
+
+const mod = num3 % num4;
+
+const max = Math.max(1, 20, 3, 4, 5);
